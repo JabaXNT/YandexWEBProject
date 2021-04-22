aa7e78acd6639fb05a59d0bd93812bc5d82d8bad
JabaXNT <gluhow2015@gmail.com>
2021-04-22 18:14:58 +0500

    repair commit

--- data/users_resource.py
+++ data/users_resource.py
@@ -1,3 +1,4 @@
+from data.user import User
 from data import db_session
 from flask import Flask, abort, jsonify
 from flask_restful import reqparse, abort, Api, Resource
@@ -7,27 +8,56 @@ api = Api(app)
 
 parser = reqparse.RequestParser()
 parser.add_argument('id')
+parser.add_argument('surname', required=True, type=str)
+parser.add_argument('name', required=True, type=str)
+parser.add_argument('email', required=True, type=str)
+parser.add_argument('hashed_password', required=True, type=str)
 
 
 class UserResource(Resource):
-    def get(self, news_id):
-        abort_if_news_not_found(news_id)
+    def get(self, user_id):
+        abort_if_news_not_found(user_id)
         session = db_session.create_session()
-        news = session.query(News).get(news_id)
-        return jsonify({'news': news.to_dict(
-            only=('title', 'content', 'user_id', 'is_private'))})
+        user = session.query(User).get(user_id)
+        return jsonify()
 
-    def delete(self, news_id):
-        abort_if_news_not_found(news_id)
+    def delete(self, user_id):
+        abort_if_news_not_found(user_id)
         session = db_session.create_session()
-        news = session.query(News).get(news_id)
+        news = session.query(User).get(user_id)
         session.delete(news)
         session.commit()
         return jsonify({'success': 'OK'})
 
 
-def abort_if_news_not_found(news_id):
+class UserListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        user = session.query(User).all()
+        return jsonify(
+            {
+                'user':
+                [item.to_dict(only=('id', 'surname', 'name', 'email', 'hashed_password'))
+                 for item in user]
+            }
+        )
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        user = User(
+            surname=args['surname'],
+            name=args['name'],
+            email=args['email'],
+        )
+        user.set_password(args['hashed_password'])
+        session.add(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+def abort_if_news_not_found(user_id):
     session = db_session.create_session()
-    news = session.query(News).get(news_id)
-    if not news:
-        abort(404, message=f"News {news_id} not found")
+    user = session.query(User).get(user_id)
+    if not user:
+        abort(404, message=f"User {user_id} not found")
